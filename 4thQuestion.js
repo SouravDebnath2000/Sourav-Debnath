ff337e83256a81b134be09ee3b85df924427e45e
SouravDebnath2000 <sourav20002332@gmail.com>
2023-06-12 10:06:41 +0530

    fruits

---
+++ 4thQuestion.js
@@ -0,0 +1,26 @@
+var message;
+var fruit = "apple";
+
+switch (fruit) {
+  case "apple":
+    message = "it is a fruit";
+    break;
+
+  case "banana":
+    message = "it is a fruit";
+    break;
+
+  case "orange":
+    message = "it is a fruit";
+    break;
+
+  case "mango":
+    message = "it is a fruit";
+    break;
+
+  default:
+    message = "not a fruit";
+    break;
+}
+
+console.log(message);
