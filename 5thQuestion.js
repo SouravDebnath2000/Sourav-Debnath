c7b7ed3b4be5059d88ec15241dde6be22ff967e4
SouravDebnath2000 <sourav20002332@gmail.com>
2023-06-12 11:16:24 +0530

    modified

--- 5thQuestion.js
+++ 5thQuestion.js
@@ -5,3 +5,7 @@ chnage2 = shoppingCart.shift();
 change3 = shoppingCart.unshift("mango");
 
 console.log(`new cart is ${shoppingCart}`);
+
+// var arr = [1, 2, 3, 4, 5];
+// arr.splice(2, 1);
+// console.log(arr);
