4b0b8bc7a0e771f2eaba149397b4e6b93a092172
SouravDebnath2000 <sourav20002332@gmail.com>
2023-06-12 10:52:33 +0530

    new

---
+++ 5thQuestion.js
@@ -0,0 +1,7 @@
+var shoppingCart = ["apple", "oranges"];
+change4 = shoppingCart.pop();
+change1 = shoppingCart.push("banana");
+chnage2 = shoppingCart.shift();
+change3 = shoppingCart.unshift("mango");
+
+console.log(`new cart is ${shoppingCart}`);
